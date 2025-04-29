b2f20cc51c50ee240421c5d19f0dbb844b0fe784
Your Name <you@example.com>
2025-04-28 23:32:03 -0400

    Fix App.js and index.js imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Chat from './components/Chat/Chat';
 import Join from './components/Join/Join';
 
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
 const App = () => {
   return (
     <Router>
-      <Route path="/" exact component={Join} />
-      <Route path="/chat" component={Chat} />
+      <Routes>
+        <Route path="/" element={<Join />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
     </Router>
   );
 }
 
 export default App;
+
