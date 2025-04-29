7bf2c6f0d0c8ab978e6938ecfb37380874576fc0
Your Name <you@example.com>
2025-04-28 23:44:55 -0400

    Your commit message

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Chat from './components/Chat/Chat';
 import Join from './components/Join/Join';
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Join />} />
-        <Route path="/chat" element={<Chat />} />
-      </Routes>
+      <Switch>
+        <Route path="/" exact component={Join} />
+        <Route path="/chat" component={Chat} />
+      </Switch>
     </Router>
   );
 }
